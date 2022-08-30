5fc2c244f125e0eee8eaf6bd9007a4c0c48bce9d
jose <carlosmendesbgi@gmail.com>
2022-08-30 17:41:27 -0300

    pagina atualizada com flexbox

---
+++ app.js
@@ -0,0 +1,19 @@
+// const express  = require('express');
+// const app = express();
+
+
+// app.listen(3000, () => {
+//     console.log("Servidor ligado");
+// })
+let arr = [11,22,33,5,77,8,99,100,1,0,33,67];
+let maior = 0;
+let menor = 0;
+
+// for(let i = 1; i < arr.length; i++) {
+//     if(arr[i] > arr[maior]) {
+//         maior = i;
+//     } else if(arr[i] < arr[menor]) {
+//         menor = i;
+//     }
+// }
+
